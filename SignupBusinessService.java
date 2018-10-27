25c749c6291235100af26fe418c04fd04f440508
PrernaPardhy <41987741+PrernaPardhy@users.noreply.github.com>
2018-10-27 18:25:02 +0530

    Changes in signupBusinessService class

---
+++ SignupBusinessService.java
@@ -0,0 +1,41 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SignupBusinessService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider cryptographyProvider;
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity signUp(final UserEntity userEntity) throws SignUpRestrictedException {
+
+
+        String[] encryptedText = cryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+
+        if (userDao.checkUserName(userEntity) != null) {
+            throw new SignUpRestrictedException("SGR-001", "Try any other username. This username has already been taken");
+        } else if (userDao.checkEmail(userEntity) != null) {
+            throw new SignUpRestrictedException("SGR-002", "This user has already been registered, try with any other emailId");
+        } else {
+            return userDao.createUser(userEntity);
+        }
+    }
+}
+
+
+
+
